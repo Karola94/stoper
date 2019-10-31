ed19a740f7ecf9cdc8e781411b1ce522bef88fb4
Karolina Kuchta <linka_94@interia.eu>
2019-10-31 12:58:27 +0100

    Stoper finished

--- js/script.js
+++ js/script.js
@@ -11,20 +11,48 @@ class Stopwatch {
             seconds: 0,
             miliseconds: 0
         };
-    }
+        this.print();      
+    }    
     print() {
         this.display.innerText = this.format(this.times);
     }
     format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }   
-    pad0(value) {
-        let result = value.toString();
-        if (result.length < 2) {
-            result = '0' + result;
+    }
+    start() {
+        if(!this.running) {
+            this.running = true;
+            this.watch = setInterval( () => this.step(), 10 );
         }
-        return result;
+    }
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
     } 
+    step() {
+        if(!this.running) return;
+        this.calculate();
+        this.print();
+    }  
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    addToList() {       
+        let listElem = document.createElement('li');
+        listElem.innerText = this.display.innerText;        
+        results.appendChild(listElem);
+    }
+    clearList() {
+        results.innerText = '';
+    }
 }
 
 const stopwatch = new Stopwatch(
@@ -36,3 +64,20 @@ startButton.addEventListener('click', () => stopwatch.start());
 let stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', () => stopwatch.stop());
 
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.reset());
+
+let results = document.querySelector('.results');
+let addToListButton = document.getElementById('addToList');
+addToListButton.addEventListener('click', () => stopwatch.addToList());
+
+let clearListButton = document.getElementById('clearList');
+clearListButton.addEventListener('click', () => stopwatch.clearList());
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+} 
